d6292e6c0b7b0d0a5469752d55357dc91f401c68
Alejandro Guerrero Gonzalez <aggftw@gmail.com>
2016-10-13 16:21:27 -0700

    Try to add ipywidgets support to notebook example

--- examples/notebook/src/index.ts
+++ examples/notebook/src/index.ts
@@ -59,8 +59,96 @@ import {
   Widget
 } from 'phosphor/lib/ui/widget';
 
+import {
+  IWidgetExtension, IDocumentContext, IDocumentModel, IDocumentRegistry
+} from 'jupyterlab/lib/docregistry';
+
+import {
+  INotebookModel
+} from 'jupyterlab/lib/notebook/notebook/model';
+
+import {
+  JupyterLabPlugin, JupyterLab
+} from 'jupyterlab/lib/application';
+
+import {
+  IKernel
+} from 'jupyter-js-services';
+
+import {
+  IDisposable, DisposableDelegate
+} from 'phosphor/lib/core/disposable';
+
+import {
+  Token
+} from 'phosphor/lib/core/token';
+
+import {
+  WidgetManager, WidgetRenderer
+} from 'jupyterlab_widgets';
+
 import 'jupyterlab/lib/default-theme/index.css';
 import '../index.css';
+
+const WIDGET_MIMETYPE = 'application/vnd.jupyter.widget';
+
+export
+const IIPyWidgetExtension = new Token<IIPyWidgetExtension>('jupyter.extensions.widgetManager');
+
+export
+interface IIPyWidgetExtension extends IPyWidgetExtension {};
+
+/**
+ * The widget manager provider.
+ */
+const widgetManagerProvider: JupyterLabPlugin<IIPyWidgetExtension> = {
+  id: 'jupyter.extensions.widgetManager',
+  provides: IIPyWidgetExtension,
+  requires: [IDocumentRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true
+};
+
+export default widgetManagerProvider;
+
+export
+class IPyWidgetExtension implements IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(nb: NotebookPanel, context: IDocumentContext<INotebookModel>): IDisposable {
+    let wManager = new WidgetManager(context, nb.content.rendermime);
+    this._registry.forEach(data => wManager.register(data));
+    let wRenderer = new WidgetRenderer(wManager);
+
+    nb.content.rendermime.addRenderer(WIDGET_MIMETYPE, wRenderer, 0);
+    return new DisposableDelegate(() => {
+      if (nb.rendermime) {
+        nb.rendermime.removeRenderer(WIDGET_MIMETYPE);
+      }
+      wRenderer.dispose();
+      wManager.dispose();
+    });
+  }
+
+  /**
+   * Register a widget module.
+   */
+  registerWidget(data: WidgetManager.IWidgetData) {
+    this._registry.push(data);
+  }
+  private _registry: WidgetManager.IWidgetData[] = [];
+}
+
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(app: JupyterLab, registry: IDocumentRegistry) {
+  let extension = new IPyWidgetExtension();
+  registry.addWidgetExtension('Notebook', extension);
+  return extension;
+}
 
 
 let NOTEBOOK = 'test.ipynb';
@@ -152,6 +240,8 @@ function createApp(manager: IServiceManager): void {
     preferKernel: true,
     canStartKernel: true
   });
+
+  activateWidgetExtension(null, docRegistry);
 
   let nbWidget = docManager.open(NOTEBOOK) as NotebookPanel;
   let palette = new CommandPalette({ commands, keymap });
